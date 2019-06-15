daa1d1405d429fe85160e75023ea709ef8f527a2
ypablo <yfedora@gmail.com>
2019-06-15 12:56:56 +0100

    27.3 Redux actions

--- actions.js
+++ actions.js
@@ -1,37 +1,48 @@
+import uuid from uuid;
+
 const ADD_COMMENT = 'ADD_COMMENT';
 const EDIT_COMMENT = 'EDIT_COMMENT';
 const REMOVE_COMMENT = 'REMOVE_COMMENT';
 const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-{
-    type: ADD_COMMENT,
-        text: 'First comment !',
-            id: uuid.v4(),
-                rating: 0
-}
-
-{
-    type: EDIT_COMMENT,
-        text: "Edited first comment",
-            id: uuid.v4(),
-                rating: rating
-}
-
-{
-    type: REMOVE_COMMENT,
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
         id: uuid.v4(),
-            rating: rating
+        rating: 0
+    }
 }
 
-{
-    type: HUMB_UP_COMMENT,
+function editComment(text) {
+    return {
+        type: EDIT_COMMENT,
+        text,
         id: uuid.v4(),
-            rating: rating + 1
+        rating: rating
+    }
 }
 
-{
-    type: HTHUMB_DOWN_COMMENT,
+function remComment(text) {
+    return {
+        type: REMOVE_COMMENT,
+        id: uuid.v4()
+    }
+}
+
+function upComment(text) {
+    return {
+        type: THUMB_UP_COMMENT,
         id: uuid.v4(),
-            rating: rating - 1
+        rating: rating + 1
+    }
+}
+
+function downComment(text) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id: uuid.v4(),
+        rating: rating - 1
+    }
 }
